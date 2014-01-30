0309afa4cc37ab24c0f96038118519de7d2f7ab6
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-30 09:59:35 -0500

    add variables to problem

--- Functions/Functions_Personal/JS/script.js
+++ Functions/Functions_Personal/JS/script.js
@@ -11,24 +11,19 @@
 var joshPizza=prompt("How many slices of Pizza does Josh eat","");
 var jonPizza=prompt("How many slices of Pizza does Jon eat","");
 
+var bigLunch=prompt("Did I have a big lunch preventing from eating a lot of pizza");
+
 var test1 =(joshPizza/8)+(jonPizza/8);
 
 
 
 //to test prompts or results
-console.log(test1);
+//console.log(test1);
 
 if(test1<=1){
     console.log("We are good on food");
+}if (joshPizza==2){
+    console.log("Josh ate two slices")
 }else{
     console.log("We have to get some more food for everyone");
 }
-
-
-
-
-
-   // if(amountCarsSold >=10){                                 //the  prompt from above will be greater or equal to 10 to get a bonus
-     //   console.log("Bonus of "+"$"+bonus+" plus you get $"+(eachCar * amountCarsSold)+ " for each car sold");  //this one took some time, but explains-
-   // }else{                                                                                                       //- the console message of achieving bonus
-    //    console.log("No bonus this month, only "+"$"+eachCar * amountCarsSold);   //The else option, if the statement above is FALSE, you get no bonus.
