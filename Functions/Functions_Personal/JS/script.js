a772239d8096266efb4f342e567fb477e8a2fd76
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-30 11:53:23 -0500

    populate fields with data for functions

--- Functions/Functions_Personal/JS/script.js
+++ Functions/Functions_Personal/JS/script.js
@@ -5,25 +5,46 @@
 
 
 
-//Problem: I am having a few friends share some pizza, I don't want to eat to much and not have enought.
+//Problem: I am having a two friends to share some pizza,I don't want us to run out of pizza....
+
+alert("I am having two friends over to share a pizza pie. Deciding if we need to order a second pizza."); //explains the problem in a alert
 
 
-var joshPizza=prompt("How many slices of Pizza does Josh eat","");
-var jonPizza=prompt("How many slices of Pizza does Jon eat","");
+var joshPizza=prompt("How many slices of Pizza does Josh want to eat","");
+var jonPizza=prompt("How many slices of Pizza does Jon want to eat","");
 
-var bigLunch=prompt("Did I have a big lunch preventing from eating a lot of pizza");
+var bigLunch=prompt("Did I have a big lunch preventing from eating a lot of pizza","yes or no");
 
-var test1 =(joshPizza/8)+(jonPizza/8);
+
+if (bigLunch=="yes"){
+    var jeremyPizza=1
+}else{
+    var jeremyPizza=3
+}
+
+var joshNachos=prompt("Did Josh bring Nachos for everyone to enjoy before pizza?","yes or no?");
+
+var total1 =(joshPizza/8)+(jonPizza/8)+(jeremyPizza/8);
+
+
+
+
+//if (joshNachos=="yes"){
+   // var total1 =   ((joshPizza-3) / 8) + ((jonPizza-3) / 8) + ((jeremyPizza-1) / 8)
+
+//}else{
+  //  var total1 =(joshPizza/8)+(jonPizza/8)+(jeremyPizza/8)
+//}
 
 
 
 //to test prompts or results
-//console.log(test1);
+//console.log(jeremyPizza);
 
-if(test1<=1){
-    console.log("We are good on food");
-}if (joshPizza==2){
-    console.log("Josh ate two slices")
-}else{
-    console.log("We have to get some more food for everyone");
+if(total1<=1){
+    console.log("We are good with one pizza pie.");
+}else if(joshNachos=="yes")
+    console.log(var total1=((joshPizza-2) / 8) + ((jonPizza-3) / 8) + ((jeremyPizza-1) / 8));
+else{
+    console.log("We have to order another pie. (and go on a diet!!");
 }
