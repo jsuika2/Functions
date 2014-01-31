4254bbf6f5eb0d5d0c8d82e1b83032e2f27c300c
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-30 19:06:35 -0500

    tested and works good, add comments

--- Functions/Functions_Personal/JS/script.js
+++ Functions/Functions_Personal/JS/script.js
@@ -9,7 +9,7 @@
 
 alert("I am having two friends over to share a pizza pie. Deciding if we need to order a second pizza."); //explains the problem in a alert
 
-var slicesPie =prompt("How many slices in your Pizza? Large =8","");
+var slicesPie =prompt("How many slices in your Pizza? Large =8","8");
 
 var joshPizza=prompt("How many slices of Pizza does Josh want to eat","");
 var jonPizza=prompt("How many slices of Pizza does Jon want to eat","");
@@ -19,13 +19,12 @@ var bigLunch=prompt("Did I have a big lunch preventing from eating pizza","yes o
 var joshNachos=prompt("Did Josh bring Nachos for everyone to enjoy before pizza?","yes or no?");
 
 
-
-
 if (bigLunch=="yes"){
-    var jeremyPizza=0
+    var jeremyPizza=0;
 }else{
-    var jeremyPizza=3
+    var jeremyPizza=3;
 }
+
 if(joshNachos=="yes"){
     var total1 =((joshPizza-1) / slicesPie)  +   ((jonPizza-2) / slicesPie)   +   ((jeremyPizza-1) / slicesPie);
 }else{
@@ -33,15 +32,10 @@ if(joshNachos=="yes"){
 }
 if(total1<=1){
  console.log("We are good with one pizza pie.");
+
 }
-else{
- console.log("We have to order another pizza pie. And go on a diet!!");
- }
-
-
-
-
-//console.log(total1);
-
-    // }else if(joshNachos=="yes")
-   // console.log("Since you filled up on nachos, you are good for one pizza.")
+else if(bigLunch=="yes" || (   (joshPizza/slicesPie)+(jonPizza/slicesPie)  <=1) ){
+    console.log("We are good on pizza because I am not eating or Josh and Jon ate less then 8 slices");
+}else{
+   console.log("We have to order another pizza pie. And go on a diet!!");
+}
